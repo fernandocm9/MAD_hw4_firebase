a6e20d03557648506e387cc34a77f1ad60d3859d
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-04-10 15:56:36 -0400

    created menu with clickable tiles

--- message_board/lib/home_page.dart
+++ message_board/lib/home_page.dart
@@ -10,15 +10,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  //Cloud Firestore should store unique user id, user first name, user last name, user
-  // role, and registration datetime
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
 
   void _signOut() async {
     await _auth.signOut();
@@ -30,23 +21,108 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('HW4: Message Board'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            tooltip: 'Sign out',
-            onPressed: _signOut,
+        title: Text('Select A Room'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children:[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Menu',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+              onTap: () {
+                // Navigate to profile
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Navigate to settings
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                _signOut();
+                Navigator.of(context).pop();
+              },
+            ),
+          ]
+        )
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              // Navigate to the selected room
+            },
+            child: Container(
+              child: Text('Games', style: TextStyle(fontSize: 24 ,color: Colors.white)),
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
           ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome, $userEmail!', style: TextStyle(fontSize: 18)),
-          ],
-        ),
-      ),
+          GestureDetector(
+            onTap: () {
+              // Navigate to the selected room
+            },
+            child: Container(
+              child: Text('Business', style: TextStyle(fontSize: 24 ,color: Colors.white)),
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              // Navigate to the selected room
+            },
+            child: Container(
+              child: Text('Public Health', style: TextStyle(fontSize: 24 ,color: Colors.white)),
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              // Navigate to the selected room
+            },
+            child: Container(
+              child: Text('Study', style: TextStyle(fontSize: 24 ,color: Colors.white)),
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+            ),
+          ),
+        ]
+      )
     );
   }
 }
